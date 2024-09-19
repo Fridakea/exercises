4e1db5bc6d3510dcf68aeebceea790e7775b9d03
Fridakea <frbe0002@stud.kea.dk>
2024-09-19 17:49:00 +0200

    momsberegner

---
+++ moms-beregner/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// Parametre defineret
+const moms = 1.25;
+const beloeb = 100;
+
+// Kalder funktionen som beregner beløbet med moms
+momsBeregner(beloeb, moms);
+
+// Selve momsberegner funktionen, som retunerer beløbet + moms
+function momsBeregner(beloeb, moms) {
+    return beloeb + moms;
+}
+
+// Console log af beløbet inkl. moms
+console.log(momsBeregner(120, moms));
