7ef0b997f966dffd1142091cfd0bed35aa666047
Fridakea <frbe0002@stud.kea.dk>
2024-09-20 09:07:04 +0200

    updates

--- moms-beregner/script.js
+++ moms-beregner/script.js
@@ -5,7 +5,7 @@ const moms = 1.25;
 const beloeb = 100;
 
 // Kalder funktionen som beregner beløbet med moms
-momsBeregner(beloeb, moms);
+momsBeregner(moms, beloeb);
 
 // Selve momsberegner funktionen, som retunerer beløbet + moms
 function momsBeregner(beloeb, moms) {
