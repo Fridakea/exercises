780fa03b4b484d7e59530deca2c5251282932e92
Fridakea <frbe0002@stud.kea.dk>
2024-09-20 14:14:24 +0200

    possible to play again

--- rock-paper-scissors-v2/index.js
+++ rock-paper-scissors-v2/index.js
@@ -1,13 +1,10 @@
 "use strict";
 
-
-
 // Variables for the players
 const userPlayer = document.getElementById("player1");
 const pcPlayer = document.getElementById("player2");
 
 // Variables for the rock, paper and scissors buttons
-const buttons = document.querySelectorAll("button");
 const rock = document.querySelector(".rock");
 const paper = document.querySelector(".paper");
 const scissors = document.querySelector(".scissors");
@@ -31,7 +28,21 @@ function getPcChoice() {
 }
 
 function startGame() {
-    console.log("startGame:", pcChoice);
+    // Reset from last game
+    document.querySelector("#play-again").classList.add("opacity-0");
+
+    rock.disabled = false;
+    paper.disabled = false;
+    scissors.disabled = false;
+
+    document.getElementById("draw").classList.add("hidden");
+    document.getElementById("win").classList.add("hidden");
+    document.getElementById("lose").classList.add("hidden");
+
+    userPlayer.classList.remove("rock", "paper", "scissors");
+    pcPlayer.classList.remove("rock", "paper", "scissors");
+
+    console.log("Pc choice:", pcChoice);
 
     // Adding eventlisteners to alle the three buttons
     rock.addEventListener("mousedown", () => handlePlayerChoice(options[0]));
@@ -48,31 +59,25 @@ function handlePlayerChoice(clickedOption) {
     document.querySelector("#player1").addEventListener("animationend", () => {
         if (playerChoice === options[0]) {
             document.querySelector("#player1").classList.remove("shake");
-            console.log("User chose rock");
             userPlayer.classList.add("rock");
         } else if (playerChoice === options[1]) {
             document.querySelector("#player1").classList.remove("shake");
-            console.log("User chose paper");
             userPlayer.classList.add("paper");
         } else {
             document.querySelector("#player1").classList.remove("shake");
-            console.log("User chose scissors");
             userPlayer.classList.add("scissors");
         }
     });
+
     document.querySelector("#player2").addEventListener("animationend", showPcChoice);
 
-    document.querySelector("#buttons").classList.add("disabled");
-
-    // Removes eventlistener and disables buttons 
-    // rock.removeEventListener("mousedown", shakeAnimationStart);
-    // paper.removeEventListener("mousedown", shakeAnimationStart);
-    // scissors.removeEventListener("mousedown", shakeAnimationStart);
+    // Disables buttons & removes eventlisteners
+    rock.disabled = true;
+    paper.disabled = true;
+    scissors.disabled = true;
 }
 
 function showPcChoice() {
-    console.log("Show pc choice", pcChoice)
-
     if (pcChoice === options[0]) {
         document.querySelector("#player2").classList.remove("shake");
         console.log("pc chose rock")
@@ -91,7 +96,6 @@ function showPcChoice() {
 }
 
 function showResult() {
-    console.log("showResult");
     if (pcChoice === playerChoice) {
         document.getElementById("draw").classList.remove("hidden");
     } else if (winningOptions[playerChoice] === pcChoice) {
@@ -99,4 +103,7 @@ function showResult() {
     } else {
         document.getElementById("lose").classList.remove("hidden");
     }
+
+    document.querySelector("#play-again").classList.remove("opacity-0");
+    document.querySelector("#play-again").addEventListener("mousedown", startGame);
 }
