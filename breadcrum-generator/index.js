2dc0b8ecb6e1d3e1eb22725d3a8aa4dfb8d5cf61
Fridakea <frbe0002@stud.kea.dk>
2024-10-04 01:48:11 +0200

    kommentarer tilføjet

--- breadcrum-generator/index.js
+++ breadcrum-generator/index.js
@@ -9,11 +9,15 @@ const bc = [
 window.addEventListener("DOMContentLoaded", generateBreadcrums);
 
 function generateBreadcrums() {
+  // Eventlistener som lytter til når knappen bliver klikket på og gør breadcrum stien "un-hidden"
   document.getElementById("generate").addEventListener("mousedown", () => {
     console.log("klikket knap");
     document.querySelector(".breadcrum").classList.remove("hidden");
   });
 
+  // Her bruger jeg .forEach til at loope over arrayet bc, og som danner breadcrumlistepunkterne vha. innerHTML.
+  // Jeg gør også brug af en konstant, seperator, som er en if statement, til at adskille listepunkterne med en /, bortset fra "last-child"
+  // --> "last-child" svarer til listens længde -1, nemlig da det første element er 0
   bc.forEach((obj) => {
     const seperator = obj === bc[bc.length - 1] ? "" : " /";
 
