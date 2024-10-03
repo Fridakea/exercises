70b91cfb3c2043e7d6673135d09537eafc0aaf6a
Fridakea <frbe0002@stud.kea.dk>
2024-10-04 01:43:33 +0200

    done

--- breadcrum-generator/index.js
+++ breadcrum-generator/index.js
@@ -6,10 +6,19 @@ const bc = [
   { name: "Bosch", link: "/hvidevarer/vaskemaskiner/bosch/" },
 ];
 
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener("DOMContentLoaded", generateBreadcrums);
+
+function generateBreadcrums() {
+  document.getElementById("generate").addEventListener("mousedown", () => {
+    console.log("klikket knap");
+    document.querySelector(".breadcrum").classList.remove("hidden");
+  });
+
   bc.forEach((obj) => {
+    const seperator = obj === bc[bc.length - 1] ? "" : " /";
+
     document.querySelector(
       ".breadcrum"
-    ).innerHTML += `<li><a href="${obj.link}">${obj.name}</a></li>`;
+    ).innerHTML += `<li><a href="${obj.link}">${obj.name}</a></li>${seperator}`;
   });
-});
+}
