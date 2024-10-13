1ff95e1cf47a707c5a98b1e9f3e83e7cf001cd56
Fridakea <frbe0002@stud.kea.dk>
2024-10-13 10:36:07 +0200

    accordions WIP

---
+++ accordions/index.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const accordionData = [
+  {
+    title: "Title 1",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  {
+    title: "Title 2",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  {
+    title: "Title 3",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+];
+
+window.addEventListener("DOMContentLoaded", () => {
+  console.log(accordionData);
+
+  accordionData.forEach((accordion) => {
+    console.log(accordion);
+
+    // // Makeing accordions using details
+    // document.querySelector(
+    //   ".accordion-section"
+    // ).innerHTML += `<details><summary>${accordion.title}</summary><p>${accordion.text}</p></details>`;
+
+    document.querySelector(
+      ".accordion-section accordion accordion-header h2"
+    ).textContent = accordion.title;
+  });
+});
