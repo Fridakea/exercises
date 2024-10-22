902bb33c5559c6e765cf4b6618adc4cbf91f3000
Fridakea <frbe0002@stud.kea.dk>
2024-10-22 20:00:31 +0200

    Muligt at lave todo's, markere todo's som done og slette todo's igen

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,17 +1,52 @@
 "use strict";
 
-let randomId = self.crypto.randomUUID();
-
-let todo = {
-  id: 0,
-  task: "-ingenting-",
-  done: false,
-};
 const todoList = [];
 
+let ul;
+
 window.addEventListener("DOMContentLoaded", () => {
+  ul = document.querySelector(".todo-list ul");
+
   document.getElementById("add-todo").addEventListener("mousedown", addTodo);
 });
+
+function drawTodoList() {
+  // clear the list before inserting the list (so the todo's only appear once)
+  ul.innerHTML = "";
+
+  todoList.forEach((object) => {
+    // instead of doing this
+    //ul.innerHTML += `<li id="${object.id}"><span>${object.task}</span><button>x</button</li>`;
+    // I do this:
+
+    // Creates li element with object.id as it's id
+    const li = document.createElement("li");
+    li.id = object.id;
+
+    // Creates span and sets it's textcontent to object.task
+    const span = document.createElement("span");
+    span.textContent = object.task;
+
+    const button = document.createElement("button");
+    button.textContent = "x";
+
+    // Creates li inside the ul, and the span and button inside the li
+    ul.appendChild(li);
+    li.appendChild(span);
+    li.appendChild(button);
+
+    span.addEventListener("mousedown", () => {
+      object.done = !object.done; // toggle true/false
+      span.style.textDecoration = object.done ? "line-through" : "none";
+    });
+
+    button.addEventListener("mousedown", () => {
+      console.log(todoList.indexOf(object));
+      todoList.splice(todoList.indexOf(object), 1);
+      drawTodoList();
+    });
+  });
+}
 
 function addTodo() {
   console.log("Todo added");
@@ -20,80 +55,13 @@ function addTodo() {
   const input = document.querySelector("#todo-string");
   const inputValue = input.value;
 
-  todo = {
-    id: randomId,
+  const todo = {
+    id: self.crypto.randomUUID(),
     task: inputValue,
+    done: false,
   };
 
   todoList.push(todo);
   console.log("todoList:", todoList);
-
-  // clear the list before inserting the list (so the todo's only appear once)
-  document.querySelector(".todo-container .todo-list ul").innerHTML = "";
-
-  todoList.forEach((element) => {
-    document.querySelector(
-      ".todo-container .todo-list ul"
-    ).innerHTML += `<li>${element.task}<button>x</button</li>`;
-
-    document
-      .querySelectorAll(".todo-container .todo-list ul li")
-      .forEach((liElement) => {
-        liElement.addEventListener("mousedown", () => {
-          if (element.done === true) {
-            console.log(element.task, "not done");
-            liElement.style.textDecoration = "none";
-
-            element.done = false;
-          } else {
-            console.log(element.task, "done");
-            liElement.style.textDecoration = "line-through";
-
-            element.done = true;
-          }
-        });
-      });
-
-    // document
-    //   .querySelector(`.${element.task}`)
-    //   .addEventListener("mousedown", () => {
-    //     console.log("slet");
-    //     console.log(todoList);
-
-    //     // todoList.splice(0, 1);
-
-    //     todoList.push(todo);
-    //   });
-  });
-
-  // document
-  //   .querySelectorAll(".todo-container .todo-list ul li")
-  //   .forEach((todoElement) => {
-  //     todoElement.addEventListener("mousedown", () => {
-  //       console.log("færdig");
-  //       console.log(todo.done);
-
-  //       if (todo.done === false) {
-  //         todoElement.style.textDecoration = "line-through";
-
-  //         todo.done = true;
-  //       } else {
-  //         todoElement.style.textDecoration = "none";
-
-  //         todo.done = false;
-  //       }
-  //     });
-
-  //     document.querySelector("span").addEventListener("mousedown", () => {
-  //       console.log("slet");
-
-  //       todoList.push(todo);
-  //     });
-  //   });
-
-  // if (todo.done === "true") {
-  //   document.querySelector(
-  //     ".todo-container .todo-done ul"
-  //   ).innerHTML += `<li>${todo.task}</li>`;
-  // }
+  drawTodoList();
 }
