0adaa7aa8f6f0be5f540a8928f1879498ebfec13
Fridakea <frbe0002@stud.kea.dk>
2024-10-22 10:49:13 +0200

    WIP

--- todo-app/index.js
+++ todo-app/index.js
@@ -2,12 +2,7 @@
 
 let randomId = self.crypto.randomUUID();
 
-const todo = {
-  id: 0,
-  task: "-no description",
-  number: 0,
-  done: false,
-};
+let todo = {};
 
 const todoList = [{ todo }];
 
@@ -17,7 +12,56 @@ window.addEventListener("DOMContentLoaded", () => {
 
 function addTodo() {
   console.log("Todo tilføjet");
+
+  const input = document.querySelector("#todo-string");
+  const inputValue = input.value;
+
+  console.log(inputValue);
+
+  todo = {
+    id: randomId,
+    task: inputValue,
+    done: false,
+  };
+
+  todoList.push(todo);
+
+  console.log(todoList);
+
+  console.log(todo);
+
   document.querySelector(
     ".todo-container .todo-list ul"
-  ).innerHTML += `<li>Hej</li>`;
+  ).innerHTML += `<li>${todo.task}<span>x</span</li>`;
+
+  document
+    .querySelectorAll(".todo-container .todo-list ul li")
+    .forEach((todoElement) => {
+      todoElement.addEventListener("mousedown", () => {
+        console.log("færdig");
+        console.log(todo.done);
+
+        if (todo.done === false) {
+          todoElement.style.textDecoration = "line-through";
+
+          todo.done = true;
+        } else {
+          todoElement.style.textDecoration = "none";
+
+          todo.done = false;
+        }
+      });
+
+      document.querySelector("span").addEventListener("mousedown", () => {
+        console.log("slet");
+
+        todoList.push(todo);
+      });
+    });
+
+  if (todo.done === "true") {
+    document.querySelector(
+      ".todo-container .todo-done ul"
+    ).innerHTML += `<li>${todo.task}</li>`;
+  }
 }
