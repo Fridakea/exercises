2a0259e1ddea2c5bfa154409fcaa58e97637d5c5
Fridakea <frbe0002@stud.kea.dk>
2024-11-04 21:23:42 +0100

    state added

--- iwatch/src/components/ProductVariantSection.jsx
+++ iwatch/src/components/ProductVariantSection.jsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useState } from 'react';
 import { twMerge } from 'tailwind-merge';
 import Image from 'next/image';
 
@@ -5,34 +8,45 @@ import navy from "../images/navy.png";
 import mint from "../images/mint.png";
 import ocean from "../images/ocean.png";
 
-export const ProductVariantSection = ({ img }) => {
-    const colorChoice = "w-5 h-5 rounded-full border-solid border-white border-2";
+export const ProductVariantSection = () => {
+    const colorChoice = "w-5 h-5 cursor-pointer rounded-full border-solid border-white border-2";
+    const selectedChoice = "w-7 h-7";
+
+    const watchChoice = "relative cursor-pointer px-5 py-2 flex-1 flex justify-center";
+
+    const [seletedWatchIndex, setSelectedWatchIndex] = useState(0);
 
     return (
         <div>
             <div className='mb-16 flex flex-row items-center'>
-                <Image src={img} alt={"alt"}/>
+                {seletedWatchIndex === 0 && <Image src={navy} alt={"Navy farvet iWatch"}/>}
+                {seletedWatchIndex === 1 && <Image src={mint} alt={"Mint farvet iWatch"}/>}
+                {seletedWatchIndex === 2 && <Image src={ocean} alt={"Ocean farvet iWatch"}/>}
+
                 <div className='flex flex-col gap-4 items-center'>
-                    <div className={twMerge(colorChoice, 'w-7 h-7 bg-[#404354]')}></div>
-                    <div className={twMerge(colorChoice, 'bg-[#58D7C4]')}></div>
-                    <div className={twMerge(colorChoice, 'bg-[#F2CEC6]')}></div>
+                    <div className={twMerge(colorChoice, 'bg-[#404354]', seletedWatchIndex === 0 && selectedChoice)}
+                     onClick={() => setSelectedWatchIndex(0)}></div>
+                    <div className={twMerge(colorChoice, 'bg-[#58D7C4]', seletedWatchIndex === 1 && selectedChoice)}
+                     onClick={() => setSelectedWatchIndex(1)}></div>
+                    <div className={twMerge(colorChoice, 'bg-[#F2CEC6]', seletedWatchIndex === 2 && selectedChoice)}
+                     onClick={() => setSelectedWatchIndex(2)}></div>
                 </div>
             </div>
 
             <div className='w-full flex flex-row gap-7'>
-                <div className='relative px-5 py-2 flex-1 flex justify-center'>
+                <div className={watchChoice} onClick={() => setSelectedWatchIndex(0)}>
                     <div className='w-full h-[65%] absolute bottom-0 bg-[rgba(67,69,88,0.6)] rounded-xl'>
                     </div>
                     <Image className='relative z-10' src={navy} alt={"Navy farvet iWatch"}/>
                 </div>
 
-                <div className='relative px-5 py-2 flex-1 flex justify-center'>
+                <div className={watchChoice} onClick={() => setSelectedWatchIndex(1)}>
                     <div className='w-full h-[65%] absolute bottom-0 bg-[rgba(106,221,204,0.6)] rounded-xl'>
                     </div>
                     <Image className='relative z-10' src={mint} alt={"Mint farvet iWatch"}/>
                 </div>
 
-                <div className='relative px-5 py-2 flex-1 flex justify-center'>
+                <div className={watchChoice} onClick={() => setSelectedWatchIndex(2)}>
                     <div className='w-full h-[65%] absolute bottom-0 bg-[rgba(249,205,196,0.6)] rounded-xl'>
                     </div>
                     <Image className='relative z-10' src={ocean} alt={"Ocean farvet iWatch"}/>
