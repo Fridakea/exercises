e47c22544a0374c8fd95387bca1a5feb4ebe759e
Fridakea <frbe0002@stud.kea.dk>
2024-11-26 12:37:05 +0100

    possible to edit and delete subscribers

--- state-url-exercise/src/app/apitest/page.jsx
+++ state-url-exercise/src/app/apitest/page.jsx
@@ -24,7 +24,7 @@ async function page() {
   console.log(subscribers);
 
   return (
-    <section className="min-h-screen max-w-[1000] mx-auto flex flex-col">
+    <section className="min-h-screen max-w-[1000] px-4 mx-auto flex flex-col">
       <form
         action={send}
         className="w-fit h-fit my-32 mx-auto px-8 py-6 bg-gray-100 rounded-xl flex flex-col items-start shadow-lg *:mb-6"
@@ -60,16 +60,16 @@ async function page() {
         <h2 className="text-2xl font-bold text-emerald-900 mb-8">
           All subscribers
         </h2>
-        <ul className="p-10 bg-gray-50 rounded-2xl grid grid-cols-3 gap-4">
+        <ul className="p-4 bg-gray-50 rounded-2xl grid grid-cols-2 gap-4 md:p-8 md:grid-cols-3">
           {subscribers.map((sub) => (
             <Link
               key={sub.id}
               href={`/update/${sub.id}`}
-              className="px-6 py-4 bg-white shadow-sm rounded-md transition-all duration-300 flex justify-between items-center hover:bg-emerald-600/10"
+              className="p-4 bg-white shadow-sm rounded-md transition-all duration-300 flex gap-4 justify-between items-center hover:bg-emerald-600/10"
             >
-              <li>
+              <li className="break-all">
                 <h3 className="text-lg">{sub.name}</h3>
-                <p className="text-sm text-black/75">{sub.email}</p>
+                <p className="text-xs text-black/75">{sub.email}</p>
               </li>
               <IoIosArrowForward />
             </Link>
--- state-url-exercise/src/lib/supabase.js
+++ state-url-exercise/src/lib/supabase.js
@@ -18,11 +18,40 @@ export async function getSubs() {
   return await response.json();
 }
 
+export async function getSubBy(id) {
+  const response = await fetch(`${subscribtionsUrl}?id=eq.${id}`, {
+    method: "GET",
+    headers: headersList,
+  });
+
+  const data = await response.json(); // array, should have length 1.
+  return data?.[0];
+}
+
 export async function postSub(subData) {
   const response = await fetch(subscribtionsUrl, {
     method: "POST",
     headers: headersList,
     body: JSON.stringify(subData),
+  });
+
+  return await response.json();
+}
+
+export async function patchSub(id, dataToUpdate) {
+  const response = await fetch(`${subscribtionsUrl}?id=eq.${id}`, {
+    method: "PATCH",
+    headers: headersList,
+    body: JSON.stringify(dataToUpdate),
+  });
+
+  return await response.json();
+}
+
+export async function deleteSub(id) {
+  const response = await fetch(`${subscribtionsUrl}?id=eq.${id}`, {
+    method: "DELETE",
+    headers: headersList,
   });
 
   return await response.json();
