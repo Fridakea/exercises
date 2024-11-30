4baf7a5d9eccda7976c0a0bc580a3f3327e1013c
Fridakea <frbe0002@stud.kea.dk>
2024-11-30 23:10:27 +0100

    WIP

--- christmas-gift-form/src/app/step-2/page.js
+++ christmas-gift-form/src/app/step-2/page.js
@@ -2,20 +2,30 @@
 
 import { Button } from "@/components/Button";
 import { LayoutWrapper } from "@/components/LayoutWrapper";
-import { useSearchParams } from "next/navigation";
+import { redirect, useSearchParams } from "next/navigation";
 
 export default function Page() {
   const searchParams = useSearchParams();
   let amount = parseInt(searchParams.get("amount"));
 
-  console.log(Array(10).fill(0));
+  function send(formData, i) {
+    const data = [
+      {
+        name: formData.getAll("name"),
+        email: formData.getAll("email"),
+        size: formData.getAll("size"),
+      },
+    ];
+
+    redirect("/step-3");
+  }
 
   return (
     <LayoutWrapper>
-      <form className="w-[85%] flex flex-col items-center">
-        <h1 className="w-10/12 mb-8 text-2xl font-bold text-green-950">
-          Udfyld detaljerne for at bestille{" "}
-          <span className="text-red-900 font-extrabold">Julenisser</span>
+      <form action={send} className="w-[85%] flex flex-col items-center">
+        <h1 className="mb-8 text-2xl font-bold text-green-950">
+          Udfyld detaljerne for at bestille
+          <span className="text-red-900 font-extrabold"> Julenisser</span>
         </h1>
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 mb-8">
           {Array(amount)
@@ -23,7 +33,7 @@ export default function Page() {
             .map((item, i) => (
               <fieldset
                 key={i}
-                className="p-4 bg-green-950/65 rounded-xl flex flex-col gap-1"
+                className="p-4 bg-green-950/50 rounded-xl flex flex-col gap-1"
               >
                 <legend className="py-2 px-4 bg-green-950 rounded-md text-white text-lg">
                   Nisse {i + 1}
--- christmas-gift-form/src/app/step-3/page.js
+++ christmas-gift-form/src/app/step-3/page.js
@@ -1,3 +1,7 @@
 export default function Page() {
-  return <div></div>;
+  return (
+    <div>
+      <p>Hej</p>
+    </div>
+  );
 }
